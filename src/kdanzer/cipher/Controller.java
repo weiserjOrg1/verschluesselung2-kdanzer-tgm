caac0b6f41dfc7a8a4cb1bfc27489d5728056f97
KaNaDaLP <kdanzer@student.tgm.ac.at>
2018-10-18 12:11:30 +0200

    V4 Layout ohne Funktion
    Das Layout des Panels wird erstellt, aber ohne, dass sie was können.

--- src/kdanzer/cipher/Controller.java
+++ src/kdanzer/cipher/Controller.java
@@ -2,8 +2,19 @@ package kdanzer.cipher;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.awt.event.WindowStateListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 
-public class Controller implements ActionListener{
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+public class Controller implements ActionListener, ChangeListener, ComponentListener {
 	/**This is the Controller which has an ActionListener implemented so the View can use Buttons
 	 * 
 	 * @author Kalian Danzer
@@ -24,4 +35,23 @@ public class Controller implements ActionListener{
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 	}
+
+	@Override
+	public void stateChanged(ChangeEvent arg0) {
+	}
+
+
+	@Override
+	public void componentResized(ComponentEvent e) { //Repaint Frame so the Components in the Pane updates
+		try {
+			this.v1.sizeAll();
+		} catch (Exception exc) {
+			System.out.println(exc);
+		}
+	}
+	
+	@Override public void componentMoved(ComponentEvent e) {}
+	@Override public void componentShown(ComponentEvent e) {}
+	@Override public void componentHidden(ComponentEvent e) {}
+	
 }
--- src/kdanzer/cipher/View.java
+++ src/kdanzer/cipher/View.java
@@ -1,13 +1,38 @@
 package kdanzer.cipher;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Toolkit;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JSpinner;
-import javax.swing.JTextField;
-import javax.swing.JTextPane;
+import javax.swing.JTextArea;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.border.Border;
+import javax.swing.border.LineBorder;
+import javax.swing.event.DocumentListener;
+import javax.swing.event.UndoableEditListener;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
+import javax.swing.text.Element;
+import javax.swing.text.PlainDocument;
+import javax.swing.text.Position;
+import javax.swing.text.Segment;
 
 public class View extends JFrame {
+	private static final long serialVersionUID = 1L;
+
 	/**This View builds a Window which makes it possible to decrypt/encrypt
 	 * and you are able to create Keys
 	 * 
@@ -16,24 +41,195 @@ public class View extends JFrame {
 	//Attribute
 	private Controller c1;
 	
-	private JTextPane verschAusgabe, entschAusgabe; //Ausgaben der verschlüsselten/entschlüsselten Texte
-	private JButton verschGo, entschGo; //Starten der Ver/Entschlüsselung
-	private JComboBox comboBox; //combobox zu auswählen der Verschlüsselung
-	private JTextField verschText, entschText, secretAlphabet;
-	private JSpinner shiftValueSpinner;
+	private JButton verschGo, entschGo, copyButton, alphabetButton; //start the Cipher
+	private JComboBox<String> comboBox; //Combobox to choose the Cipher
+	private JTextArea text, ausgabe, secretAlphabet; 	/*text field where one enter the clear text or the secret text , 
+														Returns of the clear text or the secret text
+														secretAlphabet where u enter the secrete Alphabet*/
+	private JScrollPane scrollPane1, scrollPane2;
+	private JPanel panel1, panel2, panel3; //to make the Layout
+	private JLabel label1, label2; //to be able to name some sections on the Panel;
+	private Border border1, border2;
+	private JSpinner shiftValueSpinner; //Where you rotate the secrete alphabet of shiftCipher
 	
 	//Konstruktor
 	public View(Controller c) {
-		/*Zuweisungen der Parameter*/
+		/*Initaliesing of all Parameters*/
 		this.c1 = c;
+		this.addComponentListener(this.c1);
 		
-		/*Frame Einstellungen*/
-		this.setSize(300, 200);
+
+		
+		/*Border*/
+		this.border1 = BorderFactory.createTitledBorder("Ver-/Entschlüsselter Text");
+		this.border2 = BorderFactory.createTitledBorder("Ausgabe");
+		
+		
+		/*Frame Options*/
+		this.setSize(500, 400);
+		this.setTitle("Cipher");
+		this.setMinimumSize(new Dimension(500, 400));
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLocationRelativeTo(null);
+		this.setLayout(null);
+		this.setBackground(new Color(255, 255, 255));
 		
+		
+		
+		
+		this.init();
+		this.sizeAll();
 		
 		this.setVisible(true);
+	}
+	
+	private void init() {
+		this.getContentPane().setBackground(new Color(255, 255, 255));
+		/*First Panel and his Components*/
+		
+		this.panel1 = new JPanel();
+		this.panel1.setLayout(null);
+		this.panel1.setBackground(new Color(255, 255, 255));
+		this.panel1.setBorder(this.border1);
+		this.add(panel1);
+		
+		this.text = new JTextArea();
+		this.text.setLineWrap(true);
+		this.text.setWrapStyleWord(true);
+		this.text.setBackground(new Color(230, 250, 230));
+		
+		this.scrollPane1 = new JScrollPane();
+		this.scrollPane1.setViewportView(this.text);
+		this.panel1.add(scrollPane1);
+		
+		
+		/*Second Panel and his Components*/
+		this.panel2 = new JPanel();
+		this.panel2.setLayout(null);
+		this.panel2.setBackground(new Color(255, 255, 255));
+		this.panel2.setBorder(this.border2);
+		this.add(panel2);
+		
+		this.ausgabe = new JTextArea();
+		this.ausgabe.setLineWrap(true);
+		this.ausgabe.setWrapStyleWord(true);
+		this.ausgabe.setEditable(false);
+		this.ausgabe.setBackground(new Color(206, 224, 230));
+		
+		this.scrollPane2 = new JScrollPane();
+		this.scrollPane2.setViewportView(this.ausgabe);
+		this.panel2.add(scrollPane2);
+		
+		
+		/*Buttons to start Cipher*/
+		this.verschGo = new JButton("Verschlüssen");
+		this.add(this.verschGo);
+		this.verschGo.setFocusPainted(false);
+		this.verschGo.addActionListener(this.c1);
+		
+		this.entschGo  = new JButton("Entschlüsseln");
+		this.add(this.entschGo);
+		this.entschGo.setFocusPainted(false);
+		this.entschGo.addActionListener(this.c1);
+		
+		
+		/*Copy Buton*/
+		this.copyButton  = new JButton("Ausgabe Kopieren ");
+		this.add(this.copyButton);
+		this.copyButton.setFocusPainted(false);
+		this.copyButton.addActionListener(this.c1);
+		
+		
+		/*Third Panel and his Components*/
+		this.panel3 = new JPanel();
+		this.panel3.setLayout(null);
+		this.panel3.setBackground(new Color(250, 255, 255));
+		this.add(panel3);
+		
+		this.comboBox = new JComboBox<>(new String[] {"Substitution", "Shift"});
+		this.comboBox.setFocusable(false);
+		this.comboBox.addActionListener(this.c1);
+		this.panel3.add(this.comboBox);
+		
+		this.label1 = new JLabel("Shift Wert");
+		this.panel3.add(this.label1);
+		
+		this.shiftValueSpinner = new JSpinner(new SpinnerNumberModel(0, 0, 29, 1));
+		this.shiftValueSpinner.setEditor(new JSpinner.DefaultEditor(this.shiftValueSpinner));
+		this.shiftValueSpinner.addChangeListener(this.c1);
+		this.panel3.add(this.shiftValueSpinner);
+		
+		this.secretAlphabet = new JTextArea();
+		this.secretAlphabet.setLineWrap(true);
+		this.secretAlphabet.setWrapStyleWord(true);
+		this.secretAlphabet.setBackground(new Color(231, 208, 234));
+		this.secretAlphabet.setFont(new Font("", Font.PLAIN, 15));
+		this.secretAlphabet.setBorder(BorderFactory.createLineBorder(Color.GRAY, 1));
+		this.secretAlphabet.setDocument(new PlainDocument() {
+			@Override
+			public void insertString(int offs, String str, AttributeSet a) 
+		            throws BadLocationException {
+				for (int i = str.length()-1; i >= 0; i--) {
+					String s = str.charAt(i) + "";
+			        if(s.matches("[a-zöüäß]") && secretAlphabet.getText().contains(s) == false) {
+			        	super.insertString(offs, s, a);
+			        } else {
+			        	Toolkit.getDefaultToolkit().beep();
+			        }
+				}
+		    }
+		});
+		this.panel3.add(this.secretAlphabet);
+		
+		this.alphabetButton = new JButton("Set Alphabet");
+		this.alphabetButton.setFocusPainted(false);
+		this.alphabetButton.setBorder(new LineBorder(Color.GRAY));
+		this.alphabetButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent source) {
+				alphabetButton.setBorder(BorderFactory.createEtchedBorder(0, Color.BLACK, Color.WHITE));
+			}
+			@Override
+			public void mouseExited(MouseEvent source) {
+				alphabetButton.setBorder(new LineBorder(Color.GRAY));
+			}
+		});
+		this.panel3.add(this.alphabetButton);
+		
+	}
+	
+	public void sizeAll() {
+		int realh = this.getHeight()-39;
+		/*First Panel and his Components*/
+		this.panel1.setBounds(0, 5, this.getWidth()-16, realh/4);
+		
+		this.scrollPane1.setBounds(10, this.panel1.getHeight()/4, this.panel1.getWidth()/15*14, this.panel1.getHeight()/5*3);
+		this.scrollPane1.setViewportView(this.text);
+		
+		/*Second Panel and his Components*/
+		this.panel2.setBounds(0, realh/8*5-5, this.getWidth()-16, realh/4);
+		
+		this.scrollPane2.setBounds(10, this.panel2.getHeight()/4, this.panel2.getWidth()/15*14, this.panel2.getHeight()/5*3);
+		this.scrollPane2.setViewportView(this.ausgabe);
+		
+		/*Buttons to start Cipher*/
+		int helpInt1 = realh/8*7;
+		int helpInt2 = realh - helpInt1;
+		this.verschGo.setBounds(0, helpInt1, this.getWidth()/3, helpInt2);
+		
+		this.entschGo.setBounds(this.getWidth()/3, helpInt1, this.getWidth()/3, helpInt2);
+		
+		/*Copy Buton*/
+		this.copyButton.setBounds(this.getWidth()*2/3, helpInt1, this.getWidth()/3-15, helpInt2);
+		
+		/*Third Panel and his Components*/
+		this.panel3.setBounds(0, realh/4, this.getWidth()-16, realh*3/8);
+		
+		this.comboBox.setBounds(10, 10, 150, 35);
+		this.label1.setBounds(10, 54, 80, 16);
+		this.shiftValueSpinner.setBounds(10, 70, 60, 20);
+		this.secretAlphabet.setBounds(270, 10, this.panel3.getWidth()/15*14-260, this.panel3.getHeight()/5*2);
+		this.alphabetButton.setBounds(165, 10, 100, 35);
 	}
 	
 	//Methoden
